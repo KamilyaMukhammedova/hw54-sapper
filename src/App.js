b1311000192058f43370e4497e2471c0f4026b73
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-06-05 20:42:53 +0600

    #6 Реализация работы кнопки "Reset"

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import {useState} from "react";
 import {nanoid} from "nanoid";
 import PlayingField from "./components/PlayingField/PlayingField";
 import Try from "./components/Try/Try";
+import ResetBtn from "./components/ResetBtn/ResetBtn";
 
 const App = () => {
-  const cellsArray = [];
-  for (let i = 0; i < 36; i++) {
-    cellsArray.push({
-      id: nanoid(),
-      hasItem: false,
-      isClicked: false,
-    });
-  }
-  cellsArray[Math.floor(Math.random() * cellsArray.length)].hasItem = true;
+  const generateCells = () => {
+    const cellsArray = [];
+    for (let i = 0; i < 36; i++) {
+      cellsArray.push({
+        id: nanoid(),
+        hasItem: false,
+        isClicked: false,
+      });
+    }
+    cellsArray[Math.floor(Math.random() * cellsArray.length)].hasItem = true;
+    return cellsArray;
+  };
 
-  const [cells, setCells] = useState(cellsArray);
+  const [cells, setCells] = useState(generateCells);
   const [tries, setTries] = useState(0);
 
   const onClick = cellId => {
-
     const cellsCopy = cells.map(cell => {
       if (cell.id === cellId) {
         setTries( tries + 1);
@@ -34,15 +37,24 @@ const App = () => {
     setCells(cellsCopy);
   };
 
+  const onResetGame = () => {
+     setCells(generateCells);
+     setTries(0);
+  };
+
   const playingFieldComponent = <PlayingField cells={cells} onCellClick={onClick}/>;
-  const triesComponent = <Try tries={tries}/>
+  const triesComponent = <Try tries={tries}/>;
+  const resetBtnComponent = <ResetBtn onReset={onResetGame}/>;
 
   return (
     <div className="Container">
       <div className="Field">
         {playingFieldComponent}
       </div>
-      {triesComponent}
+      <div className="CenterDiv">
+        {triesComponent}
+        {resetBtnComponent}
+      </div>
     </div>
   );
 }
--- src/components/Try/Try.js
+++ src/components/Try/Try.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import './Try.css';
 
 const Try = ({tries}) => {
   return (
-    <div className="Tries">
       <p>Tries: {tries}</p>
-    </div>
   );
 };
 
