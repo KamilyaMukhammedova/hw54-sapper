195baf7b59425d0305b78e3823ded9ef7b3e0ed0
Kamilya Mukhammedova <kamilichkam@gmail.com>
2022-06-05 21:39:12 +0600

    #7 Дополнительное задание

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {nanoid} from "nanoid";
 import PlayingField from "./components/PlayingField/PlayingField";
 import Try from "./components/Try/Try";
 import ResetBtn from "./components/ResetBtn/ResetBtn";
+import Message from "./components/Message/Message";
 
 const App = () => {
   const generateCells = () => {
@@ -20,11 +21,19 @@ const App = () => {
 
   const [cells, setCells] = useState(generateCells);
   const [tries, setTries] = useState(0);
+  const [itemIsFound, setItemIsFound] = useState(false);
+
+  let messageComponent = null;
 
   const onClick = cellId => {
     const cellsCopy = cells.map(cell => {
       if (cell.id === cellId) {
-        setTries( tries + 1);
+        if (cell.isClicked === false) {
+          setTries(tries + 1);
+        }
+        if (cell.hasItem) {
+          setItemIsFound(true);
+        }
         return {
           ...cell,
           isClicked: true,
@@ -38,9 +47,14 @@ const App = () => {
   };
 
   const onResetGame = () => {
-     setCells(generateCells);
-     setTries(0);
+    setCells(generateCells);
+    setTries(0);
+    setItemIsFound(false);
   };
+
+  if (itemIsFound === true) {
+    messageComponent = <Message/>;
+  }
 
   const playingFieldComponent = <PlayingField cells={cells} onCellClick={onClick}/>;
   const triesComponent = <Try tries={tries}/>;
@@ -48,12 +62,16 @@ const App = () => {
 
   return (
     <div className="Container">
-      <div className="Field">
+      <div
+        className="Field"
+        style={itemIsFound ? {pointerEvents: 'none', backgroundColor: '#8fbe8f'} : {pointerEvents: 'auto'}}
+      >
         {playingFieldComponent}
       </div>
       <div className="CenterDiv">
         {triesComponent}
         {resetBtnComponent}
+        {messageComponent}
       </div>
     </div>
   );
---
+++ src/components/Message/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Message.css';
+
+const Message = () => {
+  return (
+    <p className="Message">Success! The item was found!</p>
+  );
+};
+
+export default Message;
